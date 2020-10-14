75600bb3ba82736b99ba73de6fe423c3efd1f443
Alex Dolski <alexd@n-ary.net>
2020-10-14 11:47:23 -0500

    Update for latest 5.0-SNAPSHOT changes

--- src/main/java/org/example/MyJavaDelegate.java
+++ src/main/java/org/example/MyJavaDelegate.java
@@ -10,63 +10,88 @@ import java.util.Map;
 
 public class MyJavaDelegate extends AbstractJavaDelegate implements JavaDelegate {
 
+    @Override
+    public String serializeMetaIdentifier(Map<String,Object> metaIdentifier) {
+        return null;
+    }
+
+    @Override
+    public Map<String,Object> deserializeMetaIdentifier(String metaIdentifier) {
+        return null;
+    }
+
+    @Override
     public Object preAuthorize() {
         Logger.info("Hello world! The identifier is: " + getContext().getIdentifier());
         return true;
     }
 
+    @Override
     public Object authorize() {
         return true;
     }
 
+    @Override
     public Map<String,Object> getExtraIIIF2InformationResponseKeys() {
         return Collections.emptyMap();
     }
 
+    @Override
     public Map<String,Object> getExtraIIIF3InformationResponseKeys() {
         return Collections.emptyMap();
     }
 
+    @Override
     public String getSource() {
         return null;
     }
 
+    @Override
     public String getAzureStorageSourceBlobKey() {
         return null;
     }
 
+    @Override
     public String getFilesystemSourcePathname() {
         return null;
     }
 
+    @Override
     public Map<String,Object> getHTTPSourceResourceInfo() {
         return null;
     }
 
+    @Override
     public String getJDBCSourceDatabaseIdentifier() {
         return null;
     }
 
+    @Override
     public String getJDBCSourceMediaType() {
         return null;
     }
 
+    @Override
     public String getJDBCSourceLookupSQL() {
         return null;
     }
 
+    @Override
     public Map<String,String> getS3SourceObjectInfo() {
         return null;
     }
 
+    @Override
     public Map<String,Object> getOverlay() {
         return null;
     }
 
+    @Override
     public List<Map<String,Long>> getRedactions() {
         return Collections.emptyList();
     }
 
+    @Override
     public String getMetadata() {
         return null;
     }
--- src/test/java/org/example/MockJavaContext.java
+++ src/test/java/org/example/MockJavaContext.java
@@ -8,50 +8,67 @@ import java.util.Map;
 
 class MockJavaContext implements JavaContext {
 
+    @Override
     public String getClientIPAddress() {
         return "0.0.0.0";
     }
 
+    @Override
     public Map<String,String> getCookies() {
         return Collections.emptyMap();
     }
 
+    @Override
     public Map<String,Integer> getFullSize() {
         return Map.of("width", 1000, "height", 800);
     }
 
+    @Override
     public String getIdentifier() {
         return "cats";
     }
 
+    @Override
     public String getLocalURI() {
         return "http://example.org/";
     }
 
+    @Override
     public Map<String,Object> getMetadata() {
         return Collections.emptyMap();
     }
 
+    @Override
     public List<Map<String,Object>> getOperations() {
         return Collections.emptyList();
     }
 
+    @Override
     public String getOutputFormat() {
         return "image/jpeg";
     }
 
+    @Override
+    public Integer getPageNumber() {
+        return 0;
+    }
+
+    @Override
     public Map<String,String> getRequestHeaders() {
         return Collections.emptyMap();
     }
 
+    @Override
     public String getRequestURI() {
         return "http://example.org/";
     }
 
+    @Override
     public Map<String,Integer> getResultingSize() {
         return Map.of("width", 500, "height", 400);
     }
 
+    @Override
     public int[] getScaleConstraint() {
         return new int[] { 1, 1 };
     }
